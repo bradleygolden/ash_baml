82311f663eda7ee3b5920ed44a80b83cf4adb7c9
Bradley Golden <golden.bradley@gmail.com>
2025-11-23 08:02:22 -0600

    Merge test clients with umbrella config

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,10 @@
 import Config
 
+existing_clients = Application.get_env(:ash_baml, :clients, [])
+
 config :ash_baml,
-  clients: [
-    test: {AshBaml.Test.BamlClient, baml_src: "test/support/fixtures/baml_src"}
-  ]
+  clients:
+    Keyword.merge(
+      existing_clients,
+      [test: {AshBaml.Test.BamlClient, baml_src: "test/support/fixtures/baml_src"}]
+    )
