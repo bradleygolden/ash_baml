e426deb72639d28cd68d0cfbda5e813dd3fc21da
Bradley Golden <golden.bradley@gmail.com>
2025-11-23 10:32:25 -0600

    Add test client config for precommit

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,6 @@
 import Config
 
-existing_clients = Application.get_env(:ash_baml, :clients, [])
-
 config :ash_baml,
-  clients:
-    Keyword.merge(
-      existing_clients,
-      [test: {AshBaml.Test.BamlClient, baml_src: "test/support/fixtures/baml_src"}]
-    )
+  clients: [
+    test: {AshBaml.Test.BamlClient, baml_src: "test/support/fixtures/baml_src"}
+  ]
